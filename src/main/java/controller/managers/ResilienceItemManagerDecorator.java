6193cbb318ba00cc8f6772a521b67b86756563a3
MOURET Marie <m13009757@tempo21-2.luminy.univ-amu.fr>
2016-10-19 16:26:10 +0200

    Putain de bordel de merde de mille sabords de connard de programme de chiotte

--- src/main/java/controller/managers/ResilienceItemManagerDecorator.java
+++ src/main/java/controller/managers/ResilienceItemManagerDecorator.java
@@ -1,22 +1,37 @@
 package controller.managers;
 
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+
+import controller.tools.JsonTools;
 import model.api.Manager;
 import model.api.ManagerDecorator;
-import model.entity.Item;
+import model.api.ManagerListener;
+import network.api.Messages;
 import network.api.Peer;
+import network.api.SearchListener;
+import network.api.ServiceListener;
+import network.api.service.Service;
+import network.factories.AdvertisementFactory;
+import network.impl.resilient.Resilient;
+import network.api.advertisement.ResilientAdvertisementInterface;
+import network.impl.resilient.ResilientRequestService;
 
-public class ResilienceItemManagerDecorator extends ManagerDecorator<Item>{
+public class ResilienceItemManagerDecorator extends ManagerDecorator<Resilient>{
 
 	private Peer peer;
 	private String who;
-	
+
 	/**
-	 * 
+	 *
 	 * @param em Item async manager
 	 * @param peer Peer instance, started
 	 * @param who who own this instance
 	 */
-	public ResilienceItemManagerDecorator(Manager<Item> em, Peer peer, String who) {
+	public ResilienceItemManagerDecorator(Manager<Resilient> em, Peer peer, String who) {
 		super(em);
 		this.peer = peer;
 		this.who = who;
@@ -24,18 +39,18 @@ public class ResilienceItemManagerDecorator extends ManagerDecorator<Item>{
 
 
 	@Override
-	public void findAllByAttribute(String attribute, final String value, final ManagerListener<Item> l) {
+	public void findAllByAttribute(String attribute, final String value, final ManagerListener<Resilient> l) {
 		super.findAllByAttribute(attribute, value, l);
 		final ResilientRequestService resilientSender = (ResilientRequestService) peer.getService(ResilientRequestService.NAME);
 		Service resilient = peer.getService(ResilientService.NAME);
-		
+
 		resilientSender.removeListener(who);
 		resilientSender.addListener(new ServiceListener() {
-			
+
 			@Override
 			public void notify(Messages messages) {
-				JsonTools<ArrayList<Peer>> json = new JsonTools<>(new TypeReference<ArrayList<Item>>(){});
-				ArrayList<Peer> peers = json.toEntity(messages.getMessage("ResilientSxp"));
+				JsonTools<ArrayList<Resilient> json = new JsonTools<>(new TypeReference<ArrayList<Resilient>(){});
+				ArrayList<Resilient> resilient= json.toEntity(messages.getMessage("ResilientSxp"));
 				System.out.println("Discussion crée !");
 				System.out.println(messages.getMessage("ResilientSxp"));
 				for(Peer i : peers) {
@@ -43,26 +58,26 @@ public class ResilienceItemManagerDecorator extends ManagerDecorator<Item>{
 				}
 				l.notify(json.toEntity(messages.getMessage("ResilientSxp")));
 			}
-			
+
 		}, who == null ? "test":who);
-		
+
 		items.search(attribute, value, new SearchListener<ItemAdvertisementInterface>() {
 			@Override
-			public void notify(Collection<ItemAdvertisementInterface> result) {
+			public void notify(Collection<ResilientAdvertisementInterface> result) {
 				ArrayList<String> uids = new ArrayList<>();
 				for(ResilientAdvertisementInterface i: result) {
 					uids.add(i.getSourceURI());
 				}
 				resilientSender.sendRequest(value, who == null ? "test":who, uids.toArray(new String[1]));
 			}
-			
+
 		});
 	}
 
 
 
 	@Override
-	public void persist(Item entity) {
+	public void persist(Resilient entity) {
 		super.persist(entity);
 	/*	ResilientAdvertisementInterface iadv = AdvertisementFactory.createResilientAdvertisement();
 		iadv.setTitle(entity.getTitle());
@@ -70,7 +85,7 @@ public class ResilienceItemManagerDecorator extends ManagerDecorator<Item>{
 	}
 
 	@Override
-	public void begin() {
+	public void begin(Resilient entity) {
 		super.begin();
 		ResilientAdvertisementInterface iadv = AdvertisementFactory.createResilientAdvertisement();
 		iadv.setTitle(entity.getTitle());
