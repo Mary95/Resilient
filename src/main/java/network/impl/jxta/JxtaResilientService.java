a3b789d09053deb8141671516d55174eaa1daca6
marie mouret <marie mouret>
2016-12-04 21:27:26 +0100

    supression de certain commentaires

--- src/main/java/network/impl/jxta/JxtaResilientService.java
+++ src/main/java/network/impl/jxta/JxtaResilientService.java
@@ -7,22 +7,16 @@ import network.api.ResilientService;
 
 public class JxtaResilientService extends JxtaService implements ResilientService{
 
-	//private ResilientAdvertisement init;
-
 	public static final String NAME = "resilient";
 
 	public JxtaResilientService(){
 		this.name = NAME;
 	}
 
-	/*private String create_IdUnique(String key) {
-		 return java.util.Base64.getEncoder().encodeToString(key.getBytes());
-	}*/
 
 	public void initAndStart(Peer peer)
 	{
 		super.initAndStart(peer);
-		//peerUri = create_IdUnique(peerUri);
 	}
 
 }
